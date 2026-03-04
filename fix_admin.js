f71e423a34c28e3379c50f393bf025e8965832ae
Murilo Miguel <111293937+muriloitz75@users.noreply.github.com>
2026-03-04 18:59:29 -0300

    chore: limpeza de arquivos temporarios e logs antigos

--- fix_admin.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-const p = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
-p.query('UPDATE "User" SET "isAuthorized" = true, "isBlockedByAdmin" = false, "accountLocked" = false, "failedAttempts" = 0 WHERE username = $1', ['admin'])
-    .then(r => { console.log('Admin corrigido! Linhas afetadas:', r.rowCount); p.end(); })
-    .catch(e => { console.error('ERRO:', e.message); p.end(); });
--- test_db.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-const c = new Client({
-    connectionString: 'postgresql://postgres:whuZIaMvTkiXfrQKKunGoboyOCkMzfQF@turntable.proxy.rlwy.net:33986/railway',
-    ssl: { rejectUnauthorized: false }
-});
-c.connect().then(() => c.query('SELECT column_name FROM information_schema.columns WHERE table_name = \'User\';')
-    .then(r => { console.log(r.rows); c.end() }));
