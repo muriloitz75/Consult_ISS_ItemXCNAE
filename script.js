cc9c78cd2d053c1bb64c1e275728e5e5991af15a
Murilo Miguel <111293937+muriloitz75@users.noreply.github.com>
2026-03-10 13:03:48 -0300

    acessoPonto

--- script.js
+++ script.js
@@ -2182,7 +2182,7 @@ const BANNER_STATIC = {
         label: 'Justificativas de Ponto',
         menu: 'outros',
         description: 'Acesso via INTRANET sefazgo',
-        href: '#',
+        href: 'http://192.168.201.198/menu',
         icon: 'M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z',
         light: 'from-amber-50 to-orange-50 border-amber-400 hover:border-amber-500',
         dark: 'from-amber-900/40 to-orange-900/40 border-amber-500/30 hover:border-amber-400',
